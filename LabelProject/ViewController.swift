9a23a2cabc9f4cd736b4ddde98c0682b037d8dd9
Yu-Yuan Lei <YYL@yuyuansmacbook>
2017-06-21 09:39:51 -0500

    Initial Commit

---
+++ LabelProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LabelProject
+//
+//  Created by Yu-Yuan Lei on 6/21/17.
+//  Copyright © 2017 Yu-Yuan LeiDoc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
