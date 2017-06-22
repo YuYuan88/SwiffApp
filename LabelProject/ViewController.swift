a6e0eaede251a09118835e80f24e535468b8c6ac
Yu-Yuan Lei <admin@shaoboutique.com>
2017-06-22 14:08:18 -0500

    Changed tapped text.

--- LabelProject/ViewController.swift
+++ LabelProject/ViewController.swift
@@ -9,10 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var theLabel: UILabel!
+    
+    var tapCount = 0
 
+    @IBAction func buttonTapped(sender: AnyObject) {
+        
+        tapCount = tapCount + 1
+        
+        if tapCount >= 10 {
+            
+            theLabel.text = "Yu-Yuan rocks!"
+            
+        } else {
+            theLabel.text = "Oh YES!"
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
